243da8c4b52a45ae6cd6e3eabf53f8b6720543f4
Gnom204 <113632977+Gnom204@users.noreply.github.com>
2026-01-20 21:51:02 +0300

    hotfix: email fix

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
+  host: "74.125.200.109",
   port: process.env.SMTP_PORT,
   secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
   auth: {
@@ -10,10 +10,15 @@ const transporter = nodemailer.createTransport({
   },
   tls: {
     rejectUnauthorized: false,
+    servername: "smtp.gmail.com",
   },
+  family: 4,
+
   connectionTimeout: 60000, // 1 minute
   greetingTimeout: 30000,
   socketTimeout: 60000,
+  debug: true,
+  logger: true,
 });
 
 const sendEmail = async (to, subject, text) => {
